095b280b749c78cc335af19edb48f8308044a534
julefikar <julfikar.chowdhury50@myhunter.cuny.edu>
2022-07-05 21:54:53 -0400

    wrong solution, will update when it is fixed

---
+++ Other_Algorithms/1004_Max_Consecutive_Ones_III.py
@@ -0,0 +1,23 @@
+class Solution(object):
+
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        '''
+        Create a hashmap that records the values of 0 and 1, initialize them to 0. Do a sliding window.
+        WHILE the frequency of 0 is greater than k, subtract arr[windowStart] from HM and then increment wS.
+        Use the max function to record longest substring length. Return that.
+        '''
+        hm = {'0': '0', '1': '0'}
+        (windowStart, longest) = (0, 0)
+        for windowEnd in range(len(nums)):
+            right = nums[windowEnd]
+            hm[right] = hm.get(right, 0) + 1
+            while hm[int('0')] > k:
+                hm[nums[windowStart]] -= 1
+                windowStart += 1
+            longest = max(longest, windowEnd - windowStart + 1)
+        return longest
