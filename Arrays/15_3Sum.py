fd1e7d9fb2b86d05bcd65354e0cc482c03b8501b
julefikar <julfikar.chowdhury50@myhunter.cuny.edu>
2022-07-20 19:27:27 -0400

    Two pointer solution.         TC: O(N^2) Iterating through the array n^2 times.         SC: O(1) No extra DS is being used

---
+++ Arrays/15_3Sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
+        nums.sort() #Sorting the array will make the indices easier to operate two pointer solution.
+        for i in range(len(nums)-2): #-2 because there consists of three elements here, the index, left, and right pointer.
+            l = i+1
+            r = len(nums) - 1
+            while l < r:
+                currentSum = nums[i] + nums[l] + nums[r]
+                if currentSum == 0:
+                    ans.append([nums[i],nums[l],nums[r]]) #If we find that it equals to 0, append it and move l and r pointers, so case is not repeated.
+                    l += 1
+                    r -= 1
+                elif currentSum < 0: #Increment if smaller
+                    l += 1
+                else: r -= 1 #Decrement if bigger
+        actualAns = []
+        for key in ans: #Removing duplicates
+            if key not in actualAns:
+                actualAns.append(key)
+        return actualAns
