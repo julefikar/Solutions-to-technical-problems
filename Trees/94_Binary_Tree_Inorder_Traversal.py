dd60478cce69638ead9ffd562b2be436aa4a0456
julefikar <julfikar.chowdhury50@myhunter.cuny.edu>
2022-10-08 19:55:32 -0400

    In order practice

---
+++ Trees/94_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return
+        
+        ans = []
+        
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            ans.append(root.val)
+            dfs(root.right)
+        
+        dfs(root)
+        return ans
+        
+            
+            
